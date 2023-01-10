bcba1c77919d264d0dcc9707a7497bfbfc9667ad
rsahu-swi <116864784+rsahu-swi@users.noreply.github.com>
2023-01-10 08:40:53 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z "$INPUT_CLOUDFORMATION_DIRECTORY" ]; then
-  echo "Environment variable CLOUDFORMATION_DIRECTORY is not set. Quitting."
+  echo "set the cloudformation_directory input value for the action. Quitting."
   exit 1
 fi
 
@@ -11,35 +11,27 @@ if [ ! -d "$INPUT_CLOUDFORMATION_DIRECTORY" ]; then
 fi
 
 if [ -z "$INPUT_SCANNER" ]; then
-  echo "environment variable SCANNER is not set. Please use 'cfn-lint', 'cfn-nag', 'checkov', or 'all' Quitting."
+  echo "set the scanner input value for the action. Please use 'cfn-lint', 'cfn-nag', 'checkov', or 'all' Quitting."
   exit 1
 fi
 
 case $INPUT_SCANNER in
 
-  "cfn-lint")
-    echo -n "scanning with only cfn-lint"
-    sh -c "cfn-lint ${INPUT_CLOUDFORMATION_DIRECTORY}*"
+  cfn-lint|all)
+    echo -n "scanning with cfn-lint"
+    cfn-lint ${INPUT_CLOUDFORMATION_DIRECTORY}* -f sarif > cfn_lint.sarif
+    ;;&
 
-    ;;
+  cfn-nag|all)
+    echo -n "...scanning with cfn-nag"
+    cfn_nag_scan --input-path ${INPUT_CLOUDFORMATION_DIRECTORY} --output-format sarif > cfn_nag.sarif
+    ;;&
 
-  "cfn-nag")
-    echo -n "...scanning with only cfn-nag"
-    sh -c "cfn_nag_scan --input-path ${INPUT_CLOUDFORMATION_DIRECTORY}"
-    ;;
-
-  "checkov")
-    echo -n "...scanning with only checkov"
-    sh -c "checkov -d ${INPUT_CLOUDFORMATION_DIRECTORY}"
-    ;;
-
-  "all")
-    echo -n "...scanning with all tools"
-    sh -c "cfn-lint ${INPUT_CLOUDFORMATION_DIRECTORY}* -f sarif > cfn_lint.sarif"
-    sh -c "cfn_nag_scan --input-path ${INPUT_CLOUDFORMATION_DIRECTORY} --output-format sarif > cfn_nag.sarif"
-    sh -c "checkov -d ${INPUT_CLOUDFORMATION_DIRECTORY} -o sarif"
-    sh -c "mv results.sarif cfn_checkov.sarif"
-    ;;
+  checkov|all)
+    echo -n "...scanning with checkov"
+    checkov -d ${INPUT_CLOUDFORMATION_DIRECTORY} -o sarif
+    mv results.sarif cfn_checkov.sarif
+    ;;&
 
   *)
     echo -n "Environment variable SCANNER is not set allowed option. Please use 'cfn-lint', 'cfn-nag', 'checkov', or 'all' Quitting."
